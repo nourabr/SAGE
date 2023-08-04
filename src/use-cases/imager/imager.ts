b884e7d7f821ef8710b151cae74f42054f56443b
Noura <edcarlos4844@gmail.com>
2023-08-03 21:36:41 -0300

    Imager Setup

---
+++ src/use-cases/imager/imager.ts
@@ -0,0 +1,11 @@
+// Resgatar a imagem do concorrente no scrapping
+// Salver url no banco
+
+// Converter em PNG (com tamanho menor que 4mb)
+// Cortar imagem (1:1)
+// Enviar para o DALL-E
+// Receber url da imagem
+// Enviar para o wp
+// Salvar id da imagem no banco
+
+// Usar o id em featured_media no scheduler
