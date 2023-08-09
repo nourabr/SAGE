bc548f6583355c50b2117b1fffa673b977ab6631
Noura <edcarlos4844@gmail.com>
2023-08-09 20:59:51 -0300

    cache folder

--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -1,9 +1,7 @@
-const { join } = require('path')
-
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join('cache', '.cache', 'puppeteer'),
+  cacheDirectory: 'cache',
 }
