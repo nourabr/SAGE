91b3666df2a36b69ad2268c2e653473656a195a8
Noura <edcarlos4844@gmail.com>
2023-08-09 21:11:29 -0300

    render-build.sh

--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -1,7 +1,9 @@
+const { join } = require('path')
+
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: 'cache',
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 }
