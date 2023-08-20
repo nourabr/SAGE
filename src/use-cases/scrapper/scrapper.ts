b66e765a220182b2e4ff279aaa4cab462e8a2a29
Noura <edcarlos4844@gmail.com>
2023-08-20 03:11:42 -0300

    Max timeout 5minutes

--- src/use-cases/scrapper/scrapper.ts
+++ src/use-cases/scrapper/scrapper.ts
@@ -5,7 +5,7 @@ import { Competitor } from '@prisma/client'
 import puppeteer from 'puppeteer'
 
 export class Scrapper {
-  timeOutTime = 180000 // 3 Minutes
+  timeOutTime = 300000 // 5 Minutes
   successCount = 0
   lifeCycleEvent = 'load'
   async execute({
