0c618a0377992f338217fc35c55adb4b24b090ec
Noura <edcarlos4844@gmail.com>
2023-12-17 13:07:38 -0300

    gpt-turbo-16k at 6k intead of 7k

--- src/use-cases/rewriter/rewriter.ts
+++ src/use-cases/rewriter/rewriter.ts
@@ -8,7 +8,7 @@ export class Rewriter {
   async execute({ id, refTitle, refContent }: Post) {
     let gptModel = 'gpt-3.5-turbo'
 
-    if (refContent.length > 7000) {
+    if (refContent.length > 6000) {
       gptModel = 'gpt-3.5-turbo-16k'
     }
 
